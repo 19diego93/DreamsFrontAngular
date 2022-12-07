9a1c628bad85b820b3d00dd5eccf59310975f9fb
diego vallejo <vallejodiego@live.com>
2022-12-07 11:53:39 -0300

    Mock-task

---
+++ DreamFront/src/app/components/mock-task.ts
@@ -0,0 +1,25 @@
+import { Task } from "./Task"
+export const TASKS:Task[] = [
+{
+    id:1,
+    text:"terminar primer modulo",
+    day:"Agosto 5 a las 12:00",
+    reminder:true,
+},{
+    id:2,
+    text:"leer libro",
+    day:"Agosto 5 a las 14:00",
+    reminder:true,
+},{
+    id:3,
+    text:"compras",
+    day:"Agosto 5 a las 17:00",
+    reminder:false,
+},
+{
+    id:4,
+    text:"cocinar",
+    day:"Agosto 5 a las 20:00",
+    reminder:false,
+},
+]
