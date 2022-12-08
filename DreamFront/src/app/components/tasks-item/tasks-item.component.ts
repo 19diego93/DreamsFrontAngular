20548a8bbd3decee3a165dfe2337e0eecba82692
diego vallejo <vallejodiego@live.com>
2022-12-07 23:22:01 -0300

    task item component

--- DreamFront/src/app/components/tasks-item/tasks-item.component.ts
+++ DreamFront/src/app/components/tasks-item/tasks-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { Task } from '../Task';
 import { TASKS } from '../mock-task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tasks-item',
@@ -8,7 +9,7 @@ import { TASKS } from '../mock-task';
   styleUrls: ['./tasks-item.component.css']
 })
 export class TasksItemComponent implements OnInit {
-
+  faTimes = faTimes;
   @Input()task:Task = TASKS[0]
 
 constructor(){}
